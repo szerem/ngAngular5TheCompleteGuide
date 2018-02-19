8a4b7043280d2ee544eb704ff125531043492819
KSFPCR1 <szerem@tlen.pl>
2018-02-19 14:18:52 +0100

    cwiczenie 2 moje rozwiazanie

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = "";
+  allowNewUser = (this.userName.length > 0);
+  
+  onCreateUser(){
+    this.userName = ""
+    this.allowNewUser = (this.userName.length > 0);
+  }
+
+  onInput(event:any){
+    this.allowNewUser = (this.userName.length > 0);
+  }
 }
+
+
