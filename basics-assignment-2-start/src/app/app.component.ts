6a1117cba451b13830ce53ab3d04cb3fa6766768
KSFPCR1 <szerem@tlen.pl>
2018-02-19 14:25:31 +0100

    cwiczenie 2 z kursu

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -7,15 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   userName = "";
-  allowNewUser = (this.userName.length > 0);
   
-  onCreateUser(){
-    this.userName = ""
-    this.allowNewUser = (this.userName.length > 0);
-  }
-
-  onInput(event:any){
-    this.allowNewUser = (this.userName.length > 0);
+  onReset(){
+    this.userName = "";
   }
 }
 
