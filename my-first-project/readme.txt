d58674f5ecf2372d1e9b3f454cf79bed003f232b
KSFPCR1 <szerem@tlen.pl>
2018-02-24 12:11:58 +0100

    recipe-list detail item

--- my-first-project/readme.txt
+++ my-first-project/readme.txt
@@ -4,3 +4,6 @@ npm install --save bootstrap@3
 
 dodajemy resorsy do .angular-cli.json
 ng serve 
+ng g c recipes
+dodanie podfolderu 
+ng g c recipes/recipe-list --spec false
--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 
 @NgModule({
   declarations: [    
     AppComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
