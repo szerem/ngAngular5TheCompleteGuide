3cdbbd7720f9abebac2ee47512b39fe0c3c9c778
KSFPCR1 <szerem@tlen.pl>
2018-02-24 12:22:09 +0100

    shoping-list edit

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
 
 
 @NgModule({
@@ -17,7 +19,9 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    ShopingListComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule
