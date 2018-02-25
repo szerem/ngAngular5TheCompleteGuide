7f7065074caa592ffdc282f9267e735b535333cb
KSFPCR1 <szerem@tlen.pl>
2018-02-26 00:14:54 +0100

    recipe 2 ngFor

--- my-first-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-first-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipie.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe("A test recipe", "This is the test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg")
+    new Recipe("A test recipe", "This is the test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg"),
+    new Recipe("A test recipe", "This is the test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg"),
+    new Recipe("A test recipe", "This is the test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg"),
   ];
 
   constructor() { }
