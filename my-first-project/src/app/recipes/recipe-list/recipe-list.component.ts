f9a2fe07f020896130324ab76ac1b08821aab7ac
KSFPCR1 <szerem@tlen.pl>
2018-02-25 20:32:48 +0100

    recipe 1

--- my-first-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-first-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipie.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A test recipe", "This is the test", "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg")
+  ];
 
   constructor() { }
 
